b89455538c44c3e54c95e810abce71411fe2b285
FabiollaMayrink <fafamayrink@gmail.com>
2022-11-11 18:18:06 +0000

    Update DEP---CA1.R
    extra columns deleted
    
    Co-Authored-By: Henrique Demetrio <105922475+demetrio-henrique@users.noreply.github.com>

--- DEP---CA1.R
+++ DEP---CA1.R
@@ -6,3 +6,5 @@ library(stringr)
 
 dataset <- read.csv(file = "C:/Users/fafam/Downloads/datasetDEP/merged-csv-files.csv")
 head(dataset)
+
+df = subset(dataset, select = c(host_response_time , host_response_rate , host_is_superhost , host_listings_count , latitude , longitude , property_type , room_type , accommodates , bathrooms , bedrooms , beds , bed_type , amenities , price , security_deposit , cleaning_fee , guests_included , extra_people , minimum_nights , maximum_nights , number_of_reviews , review_scores_rating , review_scores_accuracy , review_scores_cleanliness , review_scores_checkin , review_scores_communication , review_scores_location , review_scores_value , instant_bookable , is_business_travel_ready , cancellation_policy))
