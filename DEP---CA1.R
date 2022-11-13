3f197e3b774e55205377da6f3829911a6d0a1941
cassedre <demetrio.henrique@outlook.com>
2022-11-13 22:32:25 +0000

    Import and consolidation of datasets
    Imported and consolidated the database for the 25 files that make up the Airbnb dataset.

--- DEP---CA1.R
+++ DEP---CA1.R
@@ -5,16 +5,30 @@ library(dplyr)
 library(stringr)
 install.packages("tidyimpute")
 library(tidyimpute)
+install.packages("os")
+library(readr)
+library(tidytext)
 
-dataset <- read.csv(file = "C:/Users/fafam/Downloads/datasetDEP/merged-csv-files.csv")
-head(dataset)
+setwd("C:/Users/USER/Downloads/dataset")
 
-df = subset(dataset, select = c(host_response_time , host_response_rate , host_is_superhost , host_listings_count , latitude , longitude , property_type , room_type , accommodates , bathrooms , bedrooms , beds , bed_type , amenities , price , security_deposit , cleaning_fee , guests_included , extra_people , minimum_nights , maximum_nights , number_of_reviews , review_scores_rating , review_scores_accuracy , review_scores_cleanliness , review_scores_checkin , review_scores_communication , review_scores_location , review_scores_value , instant_bookable , is_business_travel_ready , cancellation_policy))
-head(df)
+path_bases <- "C:/Users/USER/Downloads/dataset"
+files_months <- list.files(path_bases, pattern = "*.csv")
+print(files_months)
+quantities <- length(files_months)
+print(quantities)
+
+#
+df = NULL
+for (k in 1:quantities) {
+  
+  name = files_months[k]
+  print(name)
+  Sys.sleep(1) 
+  data = read_csv(files_months[k])
+  print(data)
+  Sys.sleep(1)
+  df = rbind(df, data.frame(name,data))
+  Sys.sleep(1)
+}
 summary(df)
 
-#esse caralho nao deu certo
-#mode(df$host_response_time)
-#mode(df$host_response_time, na.rm = TRUE)
-
-#summary(head(host_response_time))
